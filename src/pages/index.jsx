3c9e4143b671cb7f0449ca87768a04046f5d2999
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-20 00:18:14 +0200

    added 2 more dummy users

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -23,6 +23,20 @@ const HomePage = () => {
 				'https://cdn.pixabay.com/photo/2019/06/23/01/56/cityscape-4292702_1280.jpg',
 			places: 3,
 		},
+		{
+			name: 'David Smith',
+			id: 'davidsmith123',
+			image:
+				'https://cdn.pixabay.com/photo/2020/06/20/15/30/woman-doctor-5321351_1280.jpg',
+			places: 2,
+		},
+		{
+			name: 'Emily Williams',
+			id: 'emilywilliams456',
+			image:
+				'https://cdn.pixabay.com/photo/2018/01/24/19/49/friendship-day-3104635_1280.jpg',
+			places: 5,
+		},
 	];
 	return <UsersList users={DUMMY_USERS} />;
 };
