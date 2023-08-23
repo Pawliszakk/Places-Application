1af2c220729b7099016420fa1e03b87fb45f56d9
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-08-23 22:46:45 +0200

    added edit place component;

---
+++ components/places/EditPlace.jsx
@@ -0,0 +1,7 @@
+import classes from './EditPlace.module.css';
+
+const EditPlace = () => {
+	return <h1>edit Page component</h1>;
+};
+
+export default EditPlace;
--- src/pages/places/[placeId].jsx
+++ src/pages/places/[placeId].jsx
@@ -1,10 +1,16 @@
 import { useRouter } from 'next/router';
+import EditPlace from '../../../components/places/EditPlace';
 
 const PlaceEditPage = () => {
 	const router = useRouter();
 	const placeId = router.query.placeId;
 
-	return <h1>The edit page of {placeId} place, make sure you save edits!</h1>;
+	return (
+		<>
+			<h1>The edit page of {placeId} place, make sure you save edits!</h1>
+			<EditPlace />
+		</>
+	);
 };
 
 export default PlaceEditPage;
